75356552267edde1c66e63d9fd55903a3a7a3b9f
valeria <valeriamendes409@gmail.com>
2024-10-09 19:58:08 -0500

    agregando archivos

--- ACTIVIDAD-1/index.js
+++ ACTIVIDAD-1/index.js
@@ -1,10 +1,10 @@
+import { guardarTarea } from "./firebase.js";
 const formulario = document.getElementById("task-form")
 
 formulario.addEventListener('submit', (e) =>{
     e.preventDefault();
     const titulo = formulario["task-title"];
     const descripcion = formulario["task-description"];
-    console.log(titulo.value);
-    console.log(descripcion.value);
+    guardarTarea(titulo.value,descripcion.value);
     formulario.reset();
 })
